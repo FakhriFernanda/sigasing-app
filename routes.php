3dcf495023e70a448154c1f2d22b5d53d32574e6
FakhriFernanda <fakhrifernanda@gmail.com>
2023-05-31 12:23:57 +0800

    Membuat Halaman Tampil Data Karyawan

--- routes.php
+++ routes.php
@@ -49,6 +49,10 @@ if (isset($_GET['page'])){
         case 'bagiandelete':
             file_exists('pages/admin/bagiandelete.php') ? include 'pages/admin/bagiandelete.php' : include "pages/404.php";
         break;
+
+        case 'karyawanread':
+            file_exists('pages/admin/karyawanread.php') ? include 'pages/admin/karyawanread.php' : include "pages/404.php";
+        break;
     }
 } else {
     include "pages/home.php";
